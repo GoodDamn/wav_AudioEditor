603bffa51e2e7c6959c0241b5bc76d4eca3268e0
GoodDamn <76108467+GoodDamn@users.noreply.github.com>
2023-05-30 19:25:07 +0300

    Code refactoring

--- Un/Source.cpp
+++ Un/Source.cpp
@@ -5,7 +5,23 @@
 class Log {
 public:
 	Log(const char* st) {
-		std::cout << "\n" << st << "\n";
+		std::cout << "\n" << st;
+	}
+};
+
+class ShortLog
+	: public Log {
+public:
+	ShortLog(const char* st, void* val) : Log(st) {
+		std::cout << "<- " << *((int*)val) << "\n";
+	}
+};
+
+class FullLog:
+	public Log {
+public:
+	FullLog(const char* st) : Log(st) {
+		std::cout << "<- LOG:\n";
 	}
 };
 
@@ -35,50 +51,87 @@ public:
 	}
 };
 
-class AudioEditor {
+class FileFactory {
+protected:
+	std::ifstream* inputFile;
+	std::ofstream* outFile;
+
+	void writeToFile(std::ofstream* stream, int value, int size) {
+		stream->write(reinterpret_cast<const char*> (&value), size);
+	}
+
+	virtual void save(const char* st) {
+		// Empty...
+	}
+
+public:
+
+	FileFactory() {
+		// Инициализация файлового потока на чтение
+		inputFile = new std::ifstream();
+	}
+
+	~FileFactory() {
+		// При вызове деструктора, очистить RAM
+		delete inputFile;
+		if (outFile != nullptr)
+			delete outFile;
+	}
+
+	void saveFile(const char *st) {
+		if (outFile == nullptr)
+			outFile = new std::ofstream();
+		save(st);
+		outFile->close();
+		inputFile->close();
+	}
+};
+
+class AudioEditor : public FileFactory {
 public:
 	class Info {
 	public:
-		float duration = 0.0f;
-		float volume = 1.0f;
-		short channels = 1;
-		short bitDepth = 8;
-		short formatChunkSize = 16;
-		short compressionCode = 1;
-		short blockAlign = bitDepth / 8;
-		int fileSize = 0, dataSize = 0,
-			sampleRate = 48000, bitRate = sampleRate * bitDepth / 8;
+		float duration = 0.0f; // Продолжительность
+		float volume = 1.0f; // Общий уровень громкости
+		short channels = 1; // Кол-во каналов
+		short bitDepth = 8; // Глубина кодирования
+		short formatChunkSize = 16; // Размер format-секции
+		short compressionCode = 1; // Код компресии
+		short blockAlign = bitDepth / 8; // Блок-канал
+		int fileSize = 0; // Размер файла в байтах
+		int dataSize = 0; // Размер аудиоданных в байтах
+		int sampleRate = 48000; // Частота дискретизации
+		int bitRate = sampleRate * bitDepth / 8; // Частота чтения битов
 	};
 
 	class Configuration {
 	public:
-		int fadeIn = 0, fadeOut = 0;
-		int startPosition = 0, endPosition = -1;
-		float playbackSpeed = 1.0f;
-		short repeats = 1;
-		unsigned short compressSamples = 2;
+		int fadeIn = 0; // Продолжительность плавного начала песни (в миллисекундах)
+		int fadeOut = 0; // Продолжительность плавного оконачания песни (в миллисекундах)
+		int startPosition = 0; // Начальная позиция обрезки песни (в миллисекундах)
+		int endPosition = -1; // Конечная позиция обрезки песни (в миллисекундах)
+		float playbackSpeed = 1.0f; // Скорость воспроизведения 
+		short repeats = 1; // Эффект копирования (Число повторений), полезен для "зацикленных" композиций
+		unsigned short compressSamples = 2; // Уровень сжатия
 	};
 
 	Info info;
 
-	std::ifstream* inputFile;
-	std::ofstream* audioFile;
 
-	AudioEditor() {
-		inputFile = new std::ifstream();
+	// Интефейсный метод для чтения файла
+	void setDataSource(const char* path, Info* info, std::ifstream* inputFile) { 
+		setData(path, info, inputFile); 
+	}
+	void setDataSource(const char* path) {
+		setData(path, &info, inputFile);
 	}
 
-	~AudioEditor() {
-		delete inputFile;
-		if (audioFile != nullptr)
-			delete audioFile;
+	void setOutputConfiguration(Configuration configuration) {
+		this->mConfiguration = configuration;
 	}
-
-	void setDataSource(const char* path, Info* info, std::ifstream* inputFile) {setData(path,info,inputFile);}
-	void setDataSource(const char* path) {setData(path, &info, inputFile);}
 
 	void printInfo() { printInformation(&info); }
-	void printInfo(Info* info) {printInformation(info);}
+	void printInfo(Info* info) { printInformation(info); }
 
 	void uncompress(const char* inPath) {
 		std::ifstream* input = new std::ifstream();
@@ -100,12 +153,12 @@ public:
 
 		int prePos = audio->tellp();
 
-		
+
 		unsigned int current = 0;
 		std::cout << totalSamples << " - total samples\n\n";
 
 		for (unsigned int c = 0; c < totalSamples; c++) {
-			float from = getSample(input,block,maxAmp);
+			float from = getSample(input, block, maxAmp);
 			int pos = input->tellg();
 			float to = getSample(input, block, maxAmp);
 			input->seekg(pos);
@@ -135,117 +188,37 @@ public:
 		delete input, audio;
 	}
 
-	void saveFile(const char* path, Configuration config) {
-		if (audioFile == nullptr)
-			audioFile = new std::ofstream();
-
-		writeInformation(&info, audioFile, path);
-
-		short block = info.bitDepth / 8;
-
-		unsigned int samplesFadeIn = (float)config.fadeIn / 1000 * info.sampleRate; // 1500 ms
-		unsigned int samplesFadeOut = (float)config.fadeOut / 1000 * info.sampleRate; // 3500 ms
-		unsigned int totalSamples = info.sampleRate * info.duration * info.blockAlign / info.channels;
-		unsigned int trimmedSamplesFromEnd = (float)config.endPosition / 1000 * info.sampleRate;
-		std::cout << "\nTTInfo: " << info.sampleRate << " " << info.duration << " " << info.blockAlign << " " << info.channels << " " << totalSamples;
-		totalSamples = totalSamples - samplesFadeOut - trimmedSamplesFromEnd;
-		std::cout << "\nTTSamples: " << totalSamples;
-
-		float maxAmplitude = pow(2, info.bitDepth - 1) - 1;
-
-		int startPosition = config.startPosition / 1000 * info.sampleRate;
-		totalSamples -= startPosition;
-		int preAudioPos = audioFile->tellp();
-		int bytePos = startPosition * block + inputFile->tellg();
-		inputFile->seekg(bytePos);
-
-		std::cout << "\nFade in: " << samplesFadeIn
-			<< "\nFade out: " << samplesFadeOut
-			<< "\nTrimmedFromEnd: " << trimmedSamplesFromEnd
-			<< "\nBlock: " << block;
-
-		short rep = 0;
-		do {
-			std::cout << "\n" << rep << " " << samplesFadeIn << " ";
-			for (unsigned int i = 0; i < samplesFadeIn; i++) {
-				int a;
-				for (short i = 0; i < config.compressSamples; i++)
-					inputFile->read((char*)&a, block);
-				if (inputFile->eof())
-					break;
-				float totalSample = getSample(inputFile, block, maxAmplitude) * Interpolator::cubic(i, samplesFadeIn) * info.volume;
-				writeToFile(audioFile, (int)(totalSample * maxAmplitude), block);
-			}
-			std::cout << totalSamples << " ";
-			for (unsigned int i = samplesFadeIn; i < totalSamples; i++) {
-				int a;
-				for (short i = 0; i < config.compressSamples; i++)
-					inputFile->read((char*)&a, block);
-				if (inputFile->eof())
-					break;
-				writeToFile(audioFile, getSample(inputFile, block, maxAmplitude) * info.volume * maxAmplitude, block);
-			}
-			std::cout << samplesFadeOut << "\n";
-			for (unsigned int i = 0; i < samplesFadeOut; i++) {
-				int a;
-				for (short i = 0; i < config.compressSamples; i++)
-					inputFile->read((char*)&a, block);
-				if (inputFile->eof())
-					break;
-				float totalSample = getSample(inputFile, block, maxAmplitude) * (1.0f - Interpolator::linear(i, samplesFadeOut)) * info.volume;
-				writeToFile(audioFile, (int)(totalSample * maxAmplitude), block);
-			}
-
-			for (unsigned int i = 0; i < 25000; i++) {
-				writeToFile(audioFile, (int)(0.0f), block);
-			}
-			rep++;
-			inputFile->seekg(bytePos);
-		} while (rep < config.repeats);
-
-		int postAudioPos = audioFile->tellp();
-
-		audioFile->seekp(preAudioPos - 4); // Data size
-		writeToFile(audioFile, postAudioPos - preAudioPos, 4);
-
-		audioFile->seekp(4, std::ios::beg); // File size
-		writeToFile(audioFile, postAudioPos - 8, 4);
-
-		audioFile->close();
-		inputFile->close();
-
-		uncompress(path);
-	}
-
+	// Сохранение файла и применение изменений в содержимое файла
+	
 private:
-	void writeToFile(std::ofstream *stream, int value, int size) {
-		stream->write(reinterpret_cast<const char*> (&value), size);
-	}
 
-	short getDigitalSample(std::ifstream *stream,int size) {
+	Configuration mConfiguration;
+
+	short getDigitalSample(std::ifstream* stream, int size) {
 		int s;
 		stream->read((char*)&s, size);
 		return s;
 	}
 
-	float getSample(std::ifstream *stream, int size ,float maxAmp) {
-		return (float)getDigitalSample(stream,size) / maxAmp;
+	float getSample(std::ifstream* stream, int size, float maxAmp) {
+		return (float)getDigitalSample(stream, size) / maxAmp;
 	}
 
-	void printInformation(Info *info) {
-		std::cout << "\n\nFile size: " << info->fileSize
-			<< "\nSize format chunk: " << info->formatChunkSize
-			<< "\nCompresion code: " << info->compressionCode
-			<< "\nChannels: " << info->channels
-			<< "\nSample Rate per second (Hz): " << info->sampleRate
-			<< "\nBitRate per second: " << info->bitRate
-			<< "\nBlock align: " << info->blockAlign
-			<< "\nBit depth: " << info->bitDepth
-			<< "\nDuration: " << info->duration
-			<< "\nData size:" << info->dataSize << "\n\n";
+	void printInformation(Info* info) {
+		std::cout << "\n\nFILE SIZE: " << info->fileSize
+			<< "\nFORMAT CHUNK SIZE: " << info->formatChunkSize
+			<< "\nCOMPRESSION CODE: " << info->compressionCode
+			<< "\nCHANNELS: " << info->channels
+			<< "\nSAMPLE RATE PER SECOND (Hz): " << info->sampleRate
+			<< "\nBITRATE PER SECOND: " << info->bitRate
+			<< "\nBLOCK OFFSET: " << info->blockAlign
+			<< "\nBIT DEPTH: " << info->bitDepth
+			<< "\nDURATION IN SEC.: " << info->duration
+			<< "\nDATA SIZE:" << info->dataSize << "\n\n";
 	}
 
-	void writeInformation(Info *info, std::ofstream *audioFile, const char *path) {
+	// Написание конфигурации о файле с указание структуры информации, потока для сохранения, пути для сохранения
+	void writeInformation(Info* info, std::ofstream* audioFile, const char* path) {
 		audioFile->open(path, std::ios::binary | std::ios::out);
 		*audioFile << "RIFF";
 		writeToFile(audioFile, info->fileSize, 4);
@@ -267,7 +240,8 @@ private:
 		writeToFile(audioFile, info->dataSize, 4);
 	}
 
-	void setData(const char* path, Info* info, std::ifstream *inputFile) {
+	// Чтение данных с файла(путь, запись в структуры, поток чтения)
+	void setData(const char* path, Info* info, std::ifstream* inputFile) {
 		inputFile->open(path, std::ios::in | std::ios::binary);
 		if (!inputFile->is_open()) {
 			std::cout << "File doesn't exist\n";
@@ -304,12 +278,100 @@ private:
 		info->duration = (float)info->dataSize / info->bitRate;
 	}
 
+protected:
+	void save(const char* path) override {
+		// Записываем параметры файла
+		writeInformation(&info, outFile, path);
+
+		short block = info.bitDepth / 8;
+
+		unsigned int samplesFadeIn = (float)mConfiguration.fadeIn / 1000 * info.sampleRate; // 1500 ms
+		unsigned int samplesFadeOut = (float)mConfiguration.fadeOut / 1000 * info.sampleRate; // 3500 ms
+		unsigned int totalSamples = info.sampleRate * info.duration * info.blockAlign / info.channels;
+		unsigned int trimmedSamplesFromEnd = (float)mConfiguration.endPosition / 1000 * info.sampleRate;
+
+		FullLog("TOTAL INFO DURING SAVING PROCESS:");
+		ShortLog("\tSAMPLE RATE:", &info.sampleRate);
+		ShortLog("\tDURATION:", &info.duration);
+		ShortLog("\tBLOCK OFFSET:", &info.blockAlign);
+		ShortLog("\tCHANNELS:", &info.channels);
+		ShortLog("\tTOTAL SAMPLES:", &totalSamples);
+
+		totalSamples = totalSamples - samplesFadeOut - trimmedSamplesFromEnd;
+
+		ShortLog("TOTAL SAMPLES WITH TRIMMING:", &totalSamples);
+
+		float maxAmplitude = pow(2, info.bitDepth - 1) - 1;
+
+		int startPosition = mConfiguration.startPosition / 1000 * info.sampleRate;
+		totalSamples -= startPosition;
+		int preAudioPos = outFile->tellp();
+		int bytePos = startPosition * block + inputFile->tellg();
+		inputFile->seekg(bytePos);
+
+		ShortLog("FADE IN SAMPLES: ", &samplesFadeIn);
+		ShortLog("FADE OUT SAMPLES:", &samplesFadeOut);
+		ShortLog("TRIMMED SAMPLES FROM END:", &trimmedSamplesFromEnd);
+		ShortLog("BLOCK OFFSET:", &block);
+
+		short rep = 0;
+		do {
+
+			ShortLog("REPEATION", &rep);
+			ShortLog("NUMBER OF SAMPLES: ", &samplesFadeIn);
+			for (unsigned int i = 0; i < samplesFadeIn; i++) {
+				int a;
+				for (short i = 0; i < mConfiguration.compressSamples; i++)
+					inputFile->read((char*)&a, block);
+				if (inputFile->eof())
+					break;
+				float totalSample = getSample(inputFile, block, maxAmplitude) * Interpolator::cubic(i, samplesFadeIn) * info.volume;
+				writeToFile(outFile, (int)(totalSample * maxAmplitude), block);
+			}
+			ShortLog("TOTAL SAMPLES:", &totalSamples);
+			for (unsigned int i = samplesFadeIn; i < totalSamples; i++) {
+				int a;
+				for (short i = 0; i < mConfiguration.compressSamples; i++)
+					inputFile->read((char*)&a, block);
+				if (inputFile->eof())
+					break;
+				writeToFile(outFile, getSample(inputFile, block, maxAmplitude) * info.volume * maxAmplitude, block);
+			}
+			ShortLog("FADE OUT SAMPLES:",&samplesFadeOut);
+			for (unsigned int i = 0; i < samplesFadeOut; i++) {
+				int a;
+				for (short i = 0; i < mConfiguration.compressSamples; i++)
+					inputFile->read((char*)&a, block);
+				if (inputFile->eof())
+					break;
+				float totalSample = getSample(inputFile, block, maxAmplitude) * (1.0f - Interpolator::linear(i, samplesFadeOut)) * info.volume;
+				writeToFile(outFile, (int)(totalSample * maxAmplitude), block);
+			}
+
+			for (unsigned int i = 0; i < 25000; i++) {
+				writeToFile(outFile, (int)(0.0f), block);
+			}
+			rep++;
+			inputFile->seekg(bytePos);
+		} while (rep < mConfiguration.repeats);
+
+		int postAudioPos = outFile->tellp();
+
+		outFile->seekp(preAudioPos - 4); // Data size
+		writeToFile(outFile, postAudioPos - preAudioPos, 4);
+
+		outFile->seekp(4, std::ios::beg); // File size
+		writeToFile(outFile, postAudioPos - 8, 4);
+
+		//uncompress(path);
+	}
+
 };
 
 int main() {
 	std::cout << "start parsing\n";
 
-	AudioEditor *audioEditor = new AudioEditor();
+	AudioEditor* audioEditor = new AudioEditor();
 	audioEditor->setDataSource("Hymn(48000).wav");
 
 	AudioEditor::Configuration config;
@@ -319,7 +381,7 @@ int main() {
 	audioEditor->printInfo();
 	std::cout << "\n\n";
 	audioEditor->info.bitDepth = 16;
-	audioEditor->info.sampleRate = (audioEditor->info.sampleRate - audioEditor->info.sampleRate/ config.compressSamples)/ config.compressSamples; // 48000 Hz
+	audioEditor->info.sampleRate = (audioEditor->info.sampleRate - audioEditor->info.sampleRate / config.compressSamples) / config.compressSamples; // 48000 Hz
 	audioEditor->info.volume = 1.0f;
 
 	config.repeats = 1;
@@ -329,7 +391,11 @@ int main() {
 	config.startPosition = 0; // 9000 ms 
 	config.endPosition = 0;
 
-	audioEditor->saveFile("programWav.wav",config);
+	audioEditor->setOutputConfiguration(config);
+
+	audioEditor->saveFile("programWav.wav");
+
+	FullLog("EXITING PROGRAM...");
 
 	delete audioEditor;
 	return 1;
